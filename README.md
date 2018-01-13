9bfe94e02252d2f156c6d96407a8f2abf9c26c33
paulwinex <paulwinex@gmail.com>
2018-01-13 22:53:14 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django Celery example
+
+> Warning. Dun't use CELERY_ROUTES in settings.py, is'n works! Use `queue` argument in `task` decorator
+ 
+Simple example using Django+Celery
--- main/celery.py
+++ main/celery.py
@@ -2,6 +2,7 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+# create celery app
 app = Celery('clrtst')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
